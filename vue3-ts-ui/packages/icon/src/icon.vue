a93a1beff66f037a481e1f484281ea85124ac477
eternitywith <1356262689@qq.com>
2022-10-15 19:42:41 +0800

    feat(vue3-ts-ui): icon组件使用字体图标

--- vue3-ts-ui/packages/icon/src/icon.vue
+++ vue3-ts-ui/packages/icon/src/icon.vue
@@ -1,11 +1,17 @@
 <template>
-    <span>icon</span>
+    <i :class="`my-icon-${name}`"></i>
 </template>
     
-    <script lang="ts">
-    import { defineComponent } from "vue"
-    
-    export default defineComponent({
-        name: "MyIcon"
-    })
-    </script>
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "MyIcon",
+    props: {
+        name: {
+            type: String,
+            default: ''
+        }
+    }
+})
+</script>
