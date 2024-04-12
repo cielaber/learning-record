99d1d8a7e3976b4b540072d90062724f5620f1b1
eternitywith <1356262689@qq.com>
2024-04-12 14:10:36 +0800

    feat(go): tcp

---
+++ go/tcp/server/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func process(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+
+		n, err := reader.Read(buf[:])
+		fmt.Println("n：", n)
+		fmt.Println("buf：", buf)
+		if err != nil {
+			fmt.Println("read from client failed, err:", err)
+		}
+		recvStr := string(buf[:n])
+		fmt.Println("收到client端发来的数据：", recvStr)
+		conn.Write([]byte(recvStr))
+	}
+}
+
+func main() {
+	listen, err := net.Listen("tcp", "127.0.0.1:20000")
+	if err != nil {
+		fmt.Println("listen failed, err:", err)
+		return
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		}
+		go process(conn)
+	}
+}
